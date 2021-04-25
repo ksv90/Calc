d5d9fdabb38a6ff86c612e4ba0343aa0f9c8a6e9
Sergey <kaesve@yandex.ru>
2021-04-25 23:13:12 +0300

    25

--- public/calc/html.js
+++ public/calc/html.js
@@ -16,21 +16,21 @@ export default `
 		<li data-code="sqr" class="calculator__keyboard_other"><span>x<sup>2</sup></span></li>
 		<li data-code="sqrt" class="calculator__keyboard_other"><span>&#8730;</span></li>
 		<li data-code="/" class="calculator__keyboard_other"><span>/</span></li>
-		<li data-number="7"><span>7</span></li>
-		<li data-number="8"><span>8</span></li>
-		<li data-number="9"><span>9</span></li>
+		<li data-number="7" class="calculator__keyboard_number"><span>7</span></li>
+		<li data-number="8" class="calculator__keyboard_number"><span>8</span></li>
+		<li data-number="9" class="calculator__keyboard_number"><span>9</span></li>
 		<li data-code="*" class="calculator__keyboard_other"><span>*</span></li>
-		<li data-number="4"><span>4</span></li>
-		<li data-number="5"><span>5</span></li>
-		<li data-number="6"><span>6</span></li>
+		<li data-number="4" class="calculator__keyboard_number"><span>4</span></li>
+		<li data-number="5" class="calculator__keyboard_number"><span>5</span></li>
+		<li data-number="6" class="calculator__keyboard_number"><span>6</span></li>
 		<li data-code="-" class="calculator__keyboard_other"><span>-</span></li>
-		<li data-number="1"><span>1</span></li>
-		<li data-number="2"><span>2</span></li>
-		<li data-number="3"><span>3</span></li>
+		<li data-number="1" class="calculator__keyboard_number"><span>1</span></li>
+		<li data-number="2" class="calculator__keyboard_number"><span>2</span></li>
+		<li data-number="3" class="calculator__keyboard_number"><span>3</span></li>
 		<li data-code="+" class="calculator__keyboard_other"><span>+</span></li>
-		<li data-code="+/-"><span>+/-</span></li>
-		<li data-number="0"><span>0</span></li>
-		<li data-code=","><span>,</span></li>
+		<li data-code="+/-" class="calculator__keyboard_number"><span>+/-</span></li>
+		<li data-number="0" class="calculator__keyboard_number"><span>0</span></li>
+		<li data-code="," class="calculator__keyboard_number"><span>,</span></li>
 		<li data-code="=" class="calculator__keyboard_other2"><span>=</span></li>
 	</ul>
 </div>
